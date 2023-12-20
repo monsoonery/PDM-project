5916d67bb9de63fd090eb48d4c47c969653a6b08
monsoonery <84200320+monsoonery@users.noreply.github.com>
2023-12-20 11:42:33 +0100

    Improve RRT algorithm
    nodes and edges are now saved separately, with node_ids, and costs of each edge

--- python/RRT_v1.py
+++ python/RRT_v1.py
@@ -3,6 +3,7 @@ import random
 
 import matplotlib.pyplot as plt
 import ast
+import sys
 
 import obstacles
 
@@ -10,20 +11,15 @@ verbose = False
 
 class RRTstar:
     def __init__(self, l1, l2, l3):
-        self.room_variables = {
+        # dimensions of the room the robot is in
+        self.room = {
             "width": 20,
             "length": 20,
             "height": 8,
             "margin_of_closeness_to_goal": 2
         }
 
-        self.robot_variables = {
-            "l1": l1,
-            "l2": l2,
-            "l3": l3,
-            "radius_base": 0.4
-        }
-
+        # robot parameters
         self.l1 = l1
         self.l2 = l2
         self.l3 = l3
@@ -31,11 +27,18 @@ class RRTstar:
         self.all_samples = []
         self.tree = {}
 
+        self.initial_config = [0,0,0,0,0]
+        self.goal_xyz = [0,0,0]
+
         self.obstacles = obstacles.collision_obstacles
 
+        self.nodes = []
+        self.edges = []
+    
+    # returns a random sample in configuration space
     def get_random_sample(self):
-        #x = random.randint(0, self.room_variables["width"])
-        #y = random.randint(0, self.room_variables["length"])
+        #x = random.randint(0, self.room["width"])
+        #y = random.randint(0, self.room["length"])
         x = random.uniform(-10, 10)
         y = random.uniform(-10, 10)
         q1 = random.uniform(0, 2*np.pi)
@@ -43,6 +46,7 @@ class RRTstar:
         q3 = random.uniform(-2*np.pi/3, 2*np.pi/3)
         return [x,y,q1,q2,q3]
 
+    # calculate the Euclidean distance between a point (sphere) on the robot and an obstacle
     def intersects_any_obstacle(self, x, y, z, r):
         """
         Computes Euclidean distance between obstacles and robot segments, both represented 
@@ -76,6 +80,7 @@ class RRTstar:
             
         return flag
 
+    # determines if a configuration would be in collision with any objects
     def in_collision(self, config):
         """
         Using the 'union of spheres' method to calculate collisions between robot and obstacles
@@ -132,12 +137,15 @@ class RRTstar:
         # If none of the above ever return True, then there must be no collisions
         return False
     
-    def check_distance_of_two_nodes(self, node_1, node_2):
+    # calculates distance between two nodes
+    def get_distance_of_two_nodes(self, node_1, node_2):
         x1, y1, q1_node1, q2_node1, q3_node1 = node_1
         x2, y2, q1_node2, q2_node2, q3_node2 = node_2
         
         distance_xy = np.sqrt((x2-x1)**2+(y2-y1)**2)
         diff_list = [abs(q1_node1-q1_node2), abs(q2_node1-q2_node2), abs(q3_node1-q3_node2)]
+
+        # shift angle differences to be between -pi and pi
         for i in range(len(diff_list)):
             if -0.5*np.pi <= diff_list[i] <= 0.5*np.pi:
                 #print(f"{diff_list[i]} is between -90 and 90")
@@ -158,9 +166,7 @@ class RRTstar:
         distance_q1_q2 = distance_xy + sum(diff_list)
         return distance_q1_q2
     
-    def get_endpoint_coordinates(self, config):
-        return self.forward_kinematics(config, self.l1, self.l2, self.l3)
-
+    # determines the xyz-coordinates of a point on the robot
     def forward_kinematics(self, config, l1, l2, l3):
         x, y, q1, q2, q3 = config
         x_p = np.cos(q1)*(np.sin(q2)*l2 + np.cos(q2 + q3)*l3) + x
@@ -169,10 +175,15 @@ class RRTstar:
         
         return [x_p, y_p, z_p]
 
+    # determines the robot endpoint coordinates using the forward_kinematics model
+    def get_endpoint_coordinates(self, config):
+        return self.forward_kinematics(config, self.l1, self.l2, self.l3)
+
+    # checks if a configuration is near the goal
     def check_if_configuration_is_at_goal(self, config, goal):
         x1, y1, z1 = self.get_endpoint_coordinates(config)
         x2, y2, z2 = goal
-        margin = self.room_variables["margin_of_closeness_to_goal"]
+        margin = self.room["margin_of_closeness_to_goal"]
         
         # Check if the absolute differences in each dimension are within the margin
         x_close = abs(x1 - x2) <= margin
@@ -183,102 +194,127 @@ class RRTstar:
         #print(x_close, y_close, z_close)
         return x_close and y_close and z_close
 
-    def generate_graph(self, n_expansions=1000, initial_configuration=[0, 0, 0, 0, 0], goal_xyz=[0, 0, 0]):
-        #initalize start
-        self.all_samples.append(initial_configuration)
-        self.tree[str(initial_configuration)] = []
+    # Runs the RRT* algorithm to create a graph (tree)
+    def generate_graph_2(self, n_expansions=1000, initial_config=[0, 0, 0, 0, 0], goal_xyz=[0, 0, 0]):
+        self.goal_xyz = goal_xyz
+        self.initial_config = initial_config
+        
+        self.nodes = {
+            # DATA STRUCTURE: 
+            # NODE_ID: CONFIGURATION
+            0: initial_config
+        }
 
-        for i in range(n_expansions):
-            sample = self.get_random_sample()
-            if not self.in_collision(sample):
-                # TODO for loop to check if arm would be in collision 
-                # WHILE travelling from node 1 to 2
+        self.edges = {
+            # DATA STRUCTURE: 
+            # NODE_ID: [(NEXT_NODE_ID, DISTANCE/COST), (NEXT_NODE_ID, DISTANCE/COST), etc.]
+            # 0: [(1, 3.2), (2, 4.56)],
+        }
 
+        for i in range(1, n_expansions):
+            q_rand = self.get_random_sample()
+            key_q_near = min(self.nodes.keys(), key=lambda node: self.get_distance_of_two_nodes(q_rand, self.nodes[node]))
 
+            if not self.in_collision(q_rand): 
+                # TODO hier self-collision en path/travel collision check
+                if 1:
+                    # Add a new node to the node dictionary
+                    key_q_rand = i
+                    self.nodes[key_q_rand] = q_rand
 
-                #find closest node
-                closest_node = min(self.all_samples, key=lambda node: self.check_distance_of_two_nodes(sample, node))
-
-                #initialize new node to tree
-                self.tree[str(sample)] = []
-                self.all_samples.append(sample)
-
-                #reference path of closest node to new node on tree
-                self.tree[str(closest_node)].append(sample)
-
-                at_goal = self.check_if_configuration_is_at_goal(sample, goal_xyz)
+                    # Add an edge to the edges dictionary
+                    # If the nearest node doesn't have edges yet, create an entry in the dict
+                    # else append the edge entry to the existing list of edges for the nearest node
+                    if key_q_near in self.edges:
+                        self.edges[key_q_near].append((key_q_rand, self.get_distance_of_two_nodes(q_rand, self.nodes[key_q_near])))
+                    else:
+                        self.edges[key_q_near] = [(key_q_rand, self.get_distance_of_two_nodes(q_rand, self.nodes[key_q_near]))]
             else:
                 print("that sample causes collision")
+            at_goal = self.check_if_configuration_is_at_goal(q_rand, goal_xyz)
             if at_goal:
                 break
+        print(f"edges: {self.edges}")
+        print(f"nodes: {self.nodes}")
+        return self.nodes, self.edges
+
+    def plot_results_2(self):
+        # Get list of node id's (dict keys) for iterating over
+        keys_nodes = self.nodes.keys()
+
+        # Extract datapoints from node list: get x, y, etc of each node
+        x_values = [self.nodes[key_node][0] for key_node in keys_nodes]
+        y_values = [self.nodes[key_node][1] for key_node in keys_nodes]
+        q1_values = [self.nodes[key_node][2] for key_node in keys_nodes]
+        q2_values = [self.nodes[key_node][3] for key_node in keys_nodes]
+        q3_values = [self.nodes[key_node][4] for key_node in keys_nodes]
+        
+        # Plot edges
+        for key_node in self.edges.keys():
+            for edge in self.edges[key_node]:
+                key_next_node = edge[0]
+                key_current_node = key_node
+
+                next_node = self.nodes[key_current_node]
+                current_node = self.nodes[key_next_node]
+                plt.plot([current_node[0], next_node[0]], [current_node[1], next_node[1]], 'k-')
+        
+        # Plot nodes
+        plt.scatter(x_values, y_values)
+
+        # Plot shortest path
+
+        # Plot start and goal points with different colors
+        plt.scatter(x_values[0], y_values[0], color='red', label='Start Configuration')
+        plt.scatter(self.goal_xyz[0], self.goal_xyz[1], color='green', label='Goal Configuration')
+
+        # Plot goal margin
+        margin = self.room["margin_of_closeness_to_goal"]
+        square = plt.Rectangle((self.goal_xyz[0] - margin, self.goal_xyz[1] - margin), 2 * margin, 2 * margin,
+                                    color='blue', fill=False, label='Margin')
+        plt.gca().add_patch(square)
+
+        plt.title('RRT* grapth')
+        plt.xlabel('X Values')
+        plt.ylabel('Y Values')
+        plt.show()
+        return
+
+    # Extract configurations from the data
+    def extract_data(self):
+        x_values = [self.initial_config[0]]
+        y_values = [self.initial_config[1]]
+        q1_values = [self.initial_config[2]]
+        q2_values = [self.initial_config[3]]
+        q3_values = [self.initial_config[4]]
 
         for key in self.tree.keys():
-            Finalpath = []
-            if self.check_if_configuration_is_at_goal(ast.literal_eval(key), goal_xyz):
-                current_node = ast.literal_eval(key)
-                while current_node != [0, 0, 0, 0, 0]:
-                    for parentnode, children in self.tree.items():
-                        if current_node in children:
-                            print(current_node)
-                            Finalpath.insert(0, current_node)
-                            current_node = ast.literal_eval(parentnode)
+            key = ast.literal_eval(key)
+            x_values.append(key[0])
+            y_values.append(key[1]) 
+            q1_values.append(key[2])
+            q2_values.append(key[3]) 
+            q3_values.append(key[4])
 
-        return self.tree, Finalpath
+        print(x_values)
+        return x_values, y_values, q1_values, q2_values, q3_values
+
+    def dijkstra_algorithm(self):
+        pass
         
 
 if __name__ == "__main__":
     rrt = RRTstar(0.4, 0.7, 0.6)
 
-    start = [0, 0]
+    # start configuration
+    config_s = [0, 0, 0, 0, 1/2*np.pi]
     goal = [8, 8, 2]
 
-    data_dict, _ = rrt.generate_graph(n_expansions=1000, initial_configuration=[0, 0, 0, 0, 0], goal_xyz=goal)
+    #data_dict = 
+    rrt.generate_graph_2(n_expansions=1000, initial_config=config_s, goal_xyz=goal)
+    
+    #rrt.extract_data()
+    rrt.plot_results_2()
 
-    # Extract x and y values from the keys
-    x_values = []
-    y_values = []
+    #rrt.dijkstra_algorithm
 
-    for key in data_dict.keys():
-        # Assuming the first two elements are x and y
-        key = ast.literal_eval(key)
-
-        x_values.append(key[0])
-        y_values.append(key[1]) 
-
-    # Plotting
-    for key in data_dict.keys():
-        # Assuming the first two elements are x and y
-        x_and_y = ast.literal_eval(key)[:2]
-        for item in data_dict[key]:
-            s_and_z = item[:2]
-            plt.plot([x_and_y[0], s_and_z[0]], [x_and_y[1], s_and_z[1]], 'k-')
-
-    for key in data_dict.keys():
-        # Assuming the first two elements are x and y
-        if rrt.check_if_configuration_is_at_goal(ast.literal_eval(key), goal):
-            current_node = ast.literal_eval(key)
-            while current_node[:2] != start:
-                #print(f"current_node is {current_node[:2]} and not {start} so continuing")
-                for parentnode, children in data_dict.items():
-                    if current_node in children:
-                        #print(f"line from {current_node[:2]} to {ast.literal_eval(parentnode)[:2]}")
-                        plt.plot([current_node[:2][0], ast.literal_eval(parentnode)[:2][0]], [current_node[:2][1], ast.literal_eval(parentnode)[:2][1]], color='green')
-                        current_node = ast.literal_eval(parentnode)       
-
-    plt.scatter(x_values, y_values)
-
-    plt.scatter(start[0], start[1], color='red', label='Start Configuration')
-    plt.scatter(goal[0], goal[1], color='green', label='Goal Configuration')
-
-    #circle = plt.Circle((50, 50), define_space_and_robot()[0]["margin_of_closeness_to_goal"], color='blue', fill=False, label='Margin')
-    #plt.gca().add_patch(circle)
-
-    margin = rrt.room_variables["margin_of_closeness_to_goal"]
-    square = plt.Rectangle((goal[0] - margin, goal[1] - margin), 2 * margin, 2 * margin,
-                                color='blue', fill=False, label='Margin')
-    plt.gca().add_patch(square)
-
-    plt.title('Scatter Plot of Dictionary Keys')
-    plt.xlabel('X Values')
-    plt.ylabel('Y Values')
-    plt.show()
