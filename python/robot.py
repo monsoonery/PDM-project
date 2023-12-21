5fd2745b42b6c767389329aab6a0143f4a49e2bd
Quirina300 <70889288+Quirina300@users.noreply.github.com>
2023-12-21 14:54:52 +0100

    Solved going in wrong direction

--- python/robot.py
+++ python/robot.py
@@ -1,8 +1,7 @@
 import numpy as np
-import time
 
 class Robot:
-    def __init__(self, env, l1, l2, l3):
+    def __init__(self, env, initial_config, l1, l2, l3):
         # Robot size parameters
         self.l1 = l1
         self.l2 = l2
@@ -12,7 +11,7 @@ class Robot:
 
         # Robot pose variables
         vel0 = np.array([0,0,0,0,0])
-        pos0 = np.array([0,0,0,0,0])
+        pos0 = np.array(initial_config)
         self.ob, _ = self.env.reset(pos=pos0, vel=vel0)
         self.no_action = np.array([0,0,0,0,0])
 
@@ -40,22 +39,32 @@ class Robot:
 
         elif (self.position_ob[0] >= self.goal[0]-self.interval and self.position_ob[0] <= self.goal[0]+self.interval and not (self.position_ob[1] >= self.goal[1]-self.interval and self.position_ob[1] <= self.goal[1]+self.interval)):
             # print("            3")
-            if self.position_ob[1] >= self.goal[1]-self.interval: y = distance_goal[1]/time_to_reach_goal
-            elif self.position_ob[1] <= self.goal[1]+self.interval: y = distance_goal[1]/time_to_reach_goal
+            if self.position_ob[1] >= self.goal[1]+self.interval: y = -abs(distance_goal[1])/time_to_reach_goal
+            elif self.position_ob[1] <= self.goal[1]-self.interval: y = abs(distance_goal[1])/time_to_reach_goal
             else: y = 0
             x = 0
 
         elif (self.position_ob[1] >= self.goal[1]-self.interval and self.position_ob[1] <= self.goal[1]+self.interval and not (self.position_ob[0] >= self.goal[0]-self.interval and self.position_ob[0] <= self.goal[0]+self.interval)):
             # print("            4")
-            if self.position_ob[0] >= self.goal[0]-self.interval: x = distance_goal[0]/time_to_reach_goal
-            elif self.position_ob[0] <= self.goal[0]+self.interval: x = distance_goal[0]/time_to_reach_goal
+            if self.position_ob[0] >= self.goal[0]+self.interval: x = -abs(distance_goal[0])/time_to_reach_goal
+            elif self.position_ob[0] <= self.goal[0]-self.interval: x = abs(distance_goal[0])/time_to_reach_goal
             else: x = 0
             y = 0
             
         else:
+            if (self.position_ob[0] >= self.goal[0]+self.interval):
+                x = -abs(distance_goal[0])/time_to_reach_goal
+
+            elif (self.position_ob[0] <= self.goal[0]-self.interval):
+                x = abs(distance_goal[0])/time_to_reach_goal
+
+            if (self.position_ob[1] >= self.goal[1]+self.interval):
+                y = -abs(distance_goal[1])/time_to_reach_goal
+
+            elif (self.position_ob[1] <= self.goal[1]-self.interval):
+                y = abs(distance_goal[1])/time_to_reach_goal
             # print("            5")
-            x = distance_goal[0]/time_to_reach_goal
-            y = distance_goal[1]/time_to_reach_goal
+
         return x,y
     
     # Move manipulator (arm) step by step
@@ -66,11 +75,11 @@ class Robot:
             # print("            6 = ", q)
 
         elif (angle_between_goals[index] >= np.pi and not self.reached[index]):
-            q = distance_goal[index]/time_to_reach_goal
+            q = -abs(distance_goal[index])/time_to_reach_goal
             # print("            7 = ", index)
 
         elif (angle_between_goals[index] <= np.pi and not self.reached[index]):
-            q = distance_goal[index]/time_to_reach_goal
+            q = abs(distance_goal[index])/time_to_reach_goal
             # print("            8 = ", index)
         
         elif (self.reached[index] and self.position_ob[index] >= self.goal[index]+self.interval): 
@@ -91,11 +100,11 @@ class Robot:
             # print("           11 = ", index)
 
         elif (self.position_ob[index] < self.goal[index]): 
-            q = distance_goal[index]/time_to_reach_goal
+            q = abs(distance_goal[index])/time_to_reach_goal
             # print("           12 = ", index)
 
         elif (self.position_ob[index] > self.goal[index]): 
-            q = distance_goal[index]/time_to_reach_goal
+            q = -abs(distance_goal[index])/time_to_reach_goal
             # print("           13 = ", index)
 
         else: q = 0 # Should never happen
@@ -130,20 +139,20 @@ class Robot:
             angle_between_goals.append(temp)
 
         # time.sleep(1)
-        return distance_goal, angle_between_goals, largest_distance, time_to_reach_goal
+        return distance_goal, angle_between_goals, time_to_reach_goal
     
     # Main function that moves entire mobile manipulator from point one to point two (the "goal")
     def move_to_goal(self, goal):
         self.goal = goal
         self.reached = [False, False, False, False, False]
-        distance_goal, angle_between_goals, largest_distance, time_to_reach_goal = self.obtain_distances()
+        distance_goal, angle_between_goals, time_to_reach_goal = self.obtain_distances()
         
         for _ in range(10000):
             self.position_ob = self.ob["robot_0"]["joint_state"]["position"]
             self.position_ob[2] = self.angle_interval(self.position_ob[2])
             self.position_ob[3] = self.angle_interval(self.position_ob[3])
             self.position_ob[4] = self.angle_interval(self.position_ob[4])
-            # print(['%.2f' % elem for elem in self.position_ob],['%.2f' % elem for elem in self.goal])
+            print(['%.2f' % elem for elem in self.position_ob],['%.2f' % elem for elem in self.goal])
             
             if (self.position_ob[0] >= self.goal[0]-self.interval and self.position_ob[0] <= self.goal[0]+self.interval and 
                 self.position_ob[1] >= self.goal[1]-self.interval and self.position_ob[1] <= self.goal[1]+self.interval and
